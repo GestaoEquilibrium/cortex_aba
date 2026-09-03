b93538a6af7df7b15c675c0905c6113be50f2e0b
Adm Marques <gestaoequilibriummedcenter@gmail.com>
2026-09-03 11:31:20 -0300

    Corrige chave anon

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 
 const CORTEX_CONFIG = {
   SUPABASE_URL: 'https://fftmhjwnuvgzdaxpzwln.supabase.co',
-  SUPABASE_ANON_KEY: 'COLE_AQUI_A_CHAVE_ANON'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmdG1oandudXZnemRheHB6d2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNzkzMjgsImV4cCI6MjEwMDk1NTMyOH0.jG2q2iOo1DcpyZZSo6LiInzljqDaogdVH-5wTb5BhBk'
 };
 
 const sb = window.supabase.createClient(
